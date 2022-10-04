d5d418a26eee4333f0a0722939bb4f9b6cc6b626
ethanpflug <113144820+ethanpflug@users.noreply.github.com>
2022-10-04 17:23:06 -0400

    Create Excercise3.2.py

---
+++ Module3/Excercise3.2.py
@@ -0,0 +1,18 @@
+hours = input("Enter hours: ")
+
+try:
+    fh = float(hours)
+    rate = input("Enter pay rate: ")
+    try:
+        fr = float(rate)
+        if fh > 40.00 :
+            ot = fh - 40
+            otp = fr * 1.5
+            gross = ((fh - ot) * fr) + (ot * otp)
+        else : gross = fh * fr
+        print ("Pay:", gross)
+    except:
+        print("Please enter numeric input.")
+
+except:
+    print("Please enter numeric input.")
