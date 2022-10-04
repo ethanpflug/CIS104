db1b025516d511355d503b221414a081280cc43b
ethanpflug <113144820+ethanpflug@users.noreply.github.com>
2022-10-04 17:29:29 -0400

    Create Excercise2.0.py

---
+++ Module2/Excercise2.0.py
@@ -0,0 +1,16 @@
+print("At what size of a project should you start using version management software?")
+
+print("You should start using version management software as soon as you start writing code. This is because it is very important while working in teams, and also has a lot of 
+automated features which are important when you write code. Version control software usually includes automated testing, bug fixes, and deployment. There is no real reason 
+not to use version control software, since it is so easy to use in today's world. Version control software also helps you reproduce the build you have already created of 
+your software. You should use version control software throughout you coding.")
+
+
+
+
+
+
+
+
+print("Ethan Pflug")
+print('09-19-22")
