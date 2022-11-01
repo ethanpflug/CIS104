88d8fe462411208be321d1462bce3ab543d5b5e8
ethanpflug <113144820+ethanpflug@users.noreply.github.com>
2022-11-01 15:15:27 -0400

    Update Excercise7.2.py

--- Module7/Excercise7.2.py
+++ Module7/Excercise7.2.py
@@ -1,1 +1,16 @@
+fname = input("Enter file name: ")
+try:
+    fh = open(fname)
+except:
+    print('File cannot be opened')
+    quit()
 
+count = 0
+total = 0
+for line in fh:
+    if line.startswith("X-DSPAM-Confidence:"):
+        count = count + 1
+        spam = line[19:]
+        fspam = float(spam)
+        total = total + fspam
+print('Average spam confidence:', (total/count))
