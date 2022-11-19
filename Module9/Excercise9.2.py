692a6c0cf91cb04724fb7ebf1e72c0076bf74887
ethanpflug <113144820+ethanpflug@users.noreply.github.com>
2022-11-19 16:29:16 -0500

    Delete Module9 directory

--- Module9/Excercise9.2.py
+++
@@ -1,9 +0,0 @@
-name = input("Enter file:")
-handle = open(name)
-counts = dict()
-for line in handle:
-    wlist = line.split()
-    if line.startswith("From "):
-        date = wlist[2]
-        counts[date] = counts.get(date, 0) + 1
-print (counts)
