b2378a8b5c6d5553cf772fa4e7097717f42e599c
ethanpflug <113144820+ethanpflug@users.noreply.github.com>
2022-11-29 15:01:27 -0500

    Create Chapter11Autograder.py

---
+++ Module10/Chapter11/Chapter11Autograder.py
@@ -0,0 +1,17 @@
+import re
+
+hand = open('regex_sum_1648104.txt')
+numlist = []
+
+for line in hand:
+    line = line.rstrip()
+    extract = re.findall('([0-9]+)', line)
+
+    if len(extract) < 1:
+        continue
+
+    for i in range(len(extract)):
+        num = int(extract[i])
+        numlist.append(num)
+
+print(sum(numlist))
